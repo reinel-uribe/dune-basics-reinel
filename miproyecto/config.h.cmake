42b508670ca955a5d109add5f25f141a07ec326d
reinel-uribe <86264203+reinel-uribe@users.noreply.github.com>
2022-03-04 10:03:42 -0500

    Initial commit

---
+++ miproyecto/config.h.cmake
@@ -0,0 +1,45 @@
+/* begin miproyecto
+   put the definitions for config.h specific to
+   your project here. Everything above will be
+   overwritten
+*/
+
+/* begin private */
+/* Name of package */
+#define PACKAGE "@DUNE_MOD_NAME@"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "@DUNE_MAINTAINER@"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "@DUNE_MOD_NAME@"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "@DUNE_MOD_NAME@ @DUNE_MOD_VERSION@"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "@DUNE_MOD_NAME@"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "@DUNE_MOD_URL@"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "@DUNE_MOD_VERSION@"
+
+/* end private */
+
+/* Define to the version of miproyecto */
+#define MIPROYECTO_VERSION "@MIPROYECTO_VERSION@"
+
+/* Define to the major version of miproyecto */
+#define MIPROYECTO_VERSION_MAJOR @MIPROYECTO_VERSION_MAJOR@
+
+/* Define to the minor version of miproyecto */
+#define MIPROYECTO_VERSION_MINOR @MIPROYECTO_VERSION_MINOR@
+
+/* Define to the revision of miproyecto */
+#define MIPROYECTO_VERSION_REVISION @MIPROYECTO_VERSION_REVISION@
+
+/* end miproyecto
+   Everything below here will be overwritten
+*/
---
+++ miproyecto/dune/miproyecto/miproyecto.hh
@@ -0,0 +1,6 @@
+#ifndef MIPROYECTO_HH
+#define MIPROYECTO_HH
+
+// add your classes here
+
+#endif // MIPROYECTO_HH
